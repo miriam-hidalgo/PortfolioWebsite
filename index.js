4a103f4889c5072d0eb4ef3247011a47ac4756a4
Miri.Hid <mirihdz11@gmail.com>
2021-01-03 20:16:31 -0500

    updated form

---
+++ index.js
@@ -0,0 +1,6 @@
+// var myCollapsible = document.getElementById('collapseBtn')
+
+// myCollapsible.addEventListener('hidden.bs.collapse', function () {
+//   // do something...
+//   console.log(pressed)
+// })
